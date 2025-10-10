749621fff2f2f5fd89b30c8d827b8364afe0b31c
Adam Shire <adamshire123@gmail.com>
2025-10-10 14:36:10 -0400

    test custom component and styles

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,8 @@
+import { TestComponent } from "../test/test.component";
+
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
 
-
+    ["nde-search-results-container-before", TestComponent]
 
 ]);
