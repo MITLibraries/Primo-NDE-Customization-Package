955c7376f6dea5974a0e59ca5665d9e04a6bfbba
Adam Shire <adamshire123@gmail.com>
2025-10-10 14:46:09 -0400

    TITLE
    Why these changes are being introduced:
    *
    How this addresses that need:
    *
    Side effects of this change:
    *
    Relevant ticket(s):
    *

--- src/bootstrapMIT-Style.ts
+++ src/bootstrapMIT-Style.ts
@@ -1,9 +1,9 @@
 import "@angular/compiler";
 import { AppModule } from './app/app.module';
-import {bootstrap} from "@angular-architects/module-federation-tools";
+import { bootstrap } from "@angular-architects/module-federation-tools";
 
 export const bootstrapRemoteApp = (bootstrapOptions: any) => {
-   return bootstrap(AppModule(bootstrapOptions), {
+  return bootstrap(AppModule(bootstrapOptions), {
     production: true,
     appType: 'microfrontend'
   }).then(r => {
