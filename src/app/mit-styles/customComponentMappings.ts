432f0afe146af38d4aead6c55ca6ea8baf04fa7c
Adam Shire <adamshire123@gmail.com>
2025-10-15 19:23:54 -0400

    use viewencapsulation.none

--- src/app/mit-styles/customComponentMappings.ts
+++ src/app/mit-styles/customComponentMappings.ts
@@ -2,7 +2,7 @@ import { MitStylesComponent } from "./mit-styles.component";
 
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-    ['nde-search-results-container-before', MitStylesComponent]
+    ['nde-app-root-top', MitStylesComponent]
 
 
 ]);
--- src/app/mit-styles/mit-styles.component.ts
+++ src/app/mit-styles/mit-styles.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'custom-mit-styles',
   standalone: true,
   imports: [],
   templateUrl: './mit-styles.component.html',
-  styleUrl: './mit-styles.component.scss'
+  styleUrl: './mit-styles.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class MitStylesComponent implements OnInit {
   ngOnInit(): void {
