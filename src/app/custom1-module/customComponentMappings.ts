cc9bd288bc49997152b4381ec765aaa4d88a2704
Adam Shire <adamshire123@gmail.com>
2026-02-03 15:01:17 -0500

    add placeholder for library logo svg and associated markup

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,6 +1,8 @@
+import { LibraryLogoComponent } from "../library-logo/library-logo.component";
+
 // Define the map
 export const selectorComponentMap = new Map<string, any>([
-
+    ['nde-logo', LibraryLogoComponent],
 
 
 ]);
---
+++ src/app/library-logo/library-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-library-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './library-logo.component.html',
+  styleUrl: './library-logo.component.scss'
+})
+export class LibraryLogoComponent {
+
+}
