109d76e539260f73a67f340fc6bbde871e2a120f
Adam Shire <adamshire123@gmail.com>
2026-02-06 10:01:26 -0500

    refactor bootstrap process to call injectLibchat after successful app initialization

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,13 +4,15 @@ import { bootstrap } from "@angular-architects/module-federation-tools";
 import { injectLibchat } from "./app/utils/libchat";
 
 export const bootstrapRemoteApp = (bootstrapOptions: any) => {
-  injectLibchat();
 
-  const r = bootstrap(AppModule(bootstrapOptions), {
+
+  return bootstrap(AppModule(bootstrapOptions), {
     production: true,
     appType: 'microfrontend'
+  }).then(r => {
+    console.log('custom remote app bootstrap success!', r);
+    injectLibchat();
+    return r
   });
-  console.log('custom remote app bootstrap success!', r);
-  return r;
 }
 
