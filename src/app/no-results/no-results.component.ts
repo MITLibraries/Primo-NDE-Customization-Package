9464e66cdcf8663bd8240429a4e18e0abecf5ff0
Adam Shire <adamshire123@gmail.com>
2026-03-26 15:39:03 -0400

    Adds no results component

---
+++ src/app/no-results/no-results.component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+type SearchState = { searchParams: { q: string } };
+
+export const selectSearchState = createFeatureSelector<SearchState>('Search');
+export const selectSearchTerm = createSelector(
+  selectSearchState,
+  (state) => state?.searchParams?.q ?? '',
+);
+
+@Component({
+  selector: 'custom-no-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-results.component.html',
+  styleUrl: './no-results.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class NoResultsComponent {
+  private store = inject(Store);
+
+  searchTerm = toSignal(this.store.select(selectSearchTerm), {
+    initialValue: '',
+  });
+
+  worldcatUrl = computed(
+    () =>
+      `https://mit.on.worldcat.org/search?queryString=${encodeURIComponent(this.searchTerm())}`,
+  );
+}
