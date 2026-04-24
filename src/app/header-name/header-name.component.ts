83fec6786295411a90c70b7ab84cbf49ad5e759d
Adam Shire <adamshire123@gmail.com>
2026-04-24 12:23:23 -0400

    adds a component to the header to contain the branded name for Primo

---
+++ src/app/header-name/header-name.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'custom-header-name',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-name.component.html',
+  styleUrl: './header-name.component.scss'
+})
+export class HeaderNameComponent {
+
+}
