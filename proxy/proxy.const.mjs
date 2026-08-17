3614c602e5925289a0cc2fdfd0155cfd32949480
david.bendavid <david.bendavid@clarivate.com>
2026-08-17 16:40:34 +0300

    URM-3351634 | david.bendavid | Proxy default

--- proxy/proxy.const.mjs
+++ proxy/proxy.const.mjs
@@ -1,12 +1,8 @@
 //set the url of the server you want to test your code with and start the development server using the following command:
 // ng serve --proxy-config ./proxy/proxy.conf.mjs
 const environments = {
-    'example': 'https://myPrimoVE.com',
-    'sqa03-eu01': 'https://sqa03-eu01.alma.exlibrisgroup.com',
-    'sqa-na03': 'https://sqa-na03.alma.exlibrisgroup.com/',
-    'sqa-eu03': 'https://sqa-eu03.alma.exlibrisgroup.com/',
-    'sqa-cn01': 'https://sqa-cn01.alma.exlibrisgroup.com.cn/'
+    'example': 'https://myPrimoVE.com'
 };
 
-export const PROXY_TARGET = environments['sqa-cn01'];
+export const PROXY_TARGET = environments['example'];
 console.log(`[proxy] Active proxy target: ${PROXY_TARGET}`);
